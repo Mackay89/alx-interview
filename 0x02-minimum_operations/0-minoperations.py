cb66a72a9fb51ef7da5863e3263c1e2f192018c9
Mackay89 <manotomackay@gmail.com>
2024-08-08 11:18:17 -0700

    task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,15 +3,12 @@
 This module contains the function minOperations.
 """
 
-
 def minOperations(n: int) -> int:
     """
     Calculate the fewest number of operations needed to result in exactly n H characters.
-    
 
     Args:
         n (int): The number of H characters desired.
-    
 
     Returns:
         int: The minimum number of operations, or 0 if n is impossible to achieve.
@@ -19,17 +16,14 @@ def minOperations(n: int) -> int:
     if n <= 1:
         return 0
 
-    
     operations = 0
     factor = 2
 
-    
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
 
-    
     return operations
 
