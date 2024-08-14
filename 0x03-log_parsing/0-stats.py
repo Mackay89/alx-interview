d84a2a03ded86e78be4f48f728138f81dca58010
Mackay89 <manotomackay@gmail.com>
2024-08-14 11:35:23 -0700

    task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,50 @@
 #!/usr/bin/python3
+"""
+log parsing
+"""
+
 import sys
-import signal
 import re
-from collections import defaultdict
 
-# Initialize data structures
-status_codes = defaultdict(int)
-total_size = 0
-line_count = 0
 
-def signal_handler(sig, frame):
-    print_stats()
-    sys.exit(0)
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-def print_stats():
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        print(f"{code}: {status_codes[code]}")
-
-def process_line(line):
-    global total_size, line_count
-    pattern = r'(\d+\.\d+\.\d+\.\d+) - \[(.*)\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)'
-    match = re.match(pattern, line)
-    if match:
-        status_code = int(match.group(3))
-        file_size = int(match.group(4))
-        if status_code in {200, 301, 400, 401, 403, 404, 405, 500}:
-            status_codes[status_code] += 1
-            total_size += file_size
-        line_count += 1
-        if line_count % 10 == 0:
-            print_stats()
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal_handler)
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
+
     try:
         for line in sys.stdin:
-            process_line(line)
-    except KeyboardInterrupt:
-        print_stats()
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
 
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
