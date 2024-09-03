e63ebf1f19323cc80f2bfd0f5ea891185d55ebe1
Mackay89 <manotomackay@gmail.com>
2024-09-03 14:04:27 -0700

    task

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+// Import the 'node-fetch' library using CommonJS syntax
+const fetch = require('node-fetch');
+
+// Define constant with the base URL of the Star Wars API
+const API_URL = 'https://swapi-api.alx-tools.com/api';
+
+// Check if the number of command line arguments is greater than 2
+if (process.argv.length > 2) {
+  const filmId = process.argv[2];
+
+  (async () => {
+    try {
+      // Make a request to the film resource for the specified film ID
+      const response = await fetch(`${API_URL}/films/${filmId}/`);
+      const data = await response.json();
+      const charactersURL = data.characters;
+
+      // Create an array of Promises that resolve with the names of the characters
+      const characterNames = await Promise.all(
+        charactersURL.map(async (url) => {
+          const res = await fetch(url);
+          const charData = await res.json();
+          return charData.name;
+        })
+      );
+
+      // Log the names of the characters, separated by new lines
+      console.log(characterNames.join('\n'));
+    } catch (err) {
+      console.error('Error:', err.message);
+    }
+  })();
+} else {
+  console.log('Please provide a film ID as a command-line argument.');
+}
