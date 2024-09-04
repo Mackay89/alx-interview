bc50ac2a9b3dbe4bc5e7509875d999e1ee8fef3b
Mackay89 <manotomackay@gmail.com>
2024-09-04 05:19:53 -0700

    task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 // Import the 'request' library
-import request from 'request';
+const request = require('request');
 
 // Define constant with the base URL of the Star Wars API
 const API_URL = 'https://swapi-api.alx-tools.com/api';
@@ -12,47 +12,41 @@ if (process.argv.length > 2) {
 
   // Make a request to the film resource for the specified film ID
   request(`${API_URL}/films/${filmId}/`, (err, _, body) => {
-    // If an error occurred during the request, log the error
     if (err) {
       console.error(`Error fetching film data: ${err.message}`);
       return;
     }
 
     try {
-      // Parse the response body to get the characters URL
-      const charactersURL = JSON.parse(body).characters;
+      // Get the characters URL from the film's response body
+      const charactersURLs = JSON.parse(body).characters;
 
       // Create an array of Promises that resolve with the names of the characters
-      const charactersName = charactersURL.map(url => new Promise((resolve, reject) => {
-        // Make a request to the character resource
-        request(url, (promiseErr, __, charactersReqBody) => {
-          // If an error occurred during the request, reject the Promise with the error
-          if (promiseErr) {
-            reject(`Error fetching character data: ${promiseErr.message}`);
-            return;
-          }
-          
-          try {
-            // Parse the character response body
-            const characterName = JSON.parse(charactersReqBody).name;
-            resolve(characterName);
-          } catch (parseErr) {
-            reject(`Error parsing character data: ${parseErr.message}`);
-          }
-        });
-      }));
+      const characterPromises = charactersURLs.map(url =>
+        new Promise((resolve, reject) => {
+          request(url, (promiseErr, __, charactersReqBody) => {
+            if (promiseErr) {
+              reject(`Error fetching character data: ${promiseErr.message}`);
+              return;
+            }
 
-      // Wait for all Promises to resolve and then print the character names
-      Promise.all(charactersName)
-        .then(names => {
-          console.log(names.join('\n'));
+            try {
+              const characterName = JSON.parse(charactersReqBody).name;
+              resolve(characterName);
+            } catch (parseError) {
+              reject(`Error parsing character data: ${parseError.message}`);
+            }
+          });
         })
-        .catch(error => {
-          console.error(`Error in fetching character names: ${error}`);
-        });
+      );
 
-    } catch (parseErr) {
-      console.error(`Error parsing film data: ${parseErr.message}`);
+      // Wait for all Promises to resolve and log the names of the characters, separated by new lines
+      Promise.all(characterPromises)
+        .then(names => console.log(names.join('\n')))
+        .catch(promiseError => console.error(`Error in fetching character names: ${promiseError.message}`));
+
+    } catch (parseError) {
+      console.error(`Error parsing film data: ${parseError.message}`);
     }
   });
 }
